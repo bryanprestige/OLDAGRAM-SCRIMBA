fe7be22d5b320c7b17391fa5eef1f802d4e650d7
Bryanprestige <bryanhvaldez@gmail.com>
2025-03-18 12:14:03 +0100

    feat: oldagram finished
    oladragm finished

--- src/js/component/OldagramPost.js
+++ src/js/component/OldagramPost.js
@@ -38,23 +38,22 @@ export class OldagramPost extends LitElement {
                     <h2>${this.location}</h1>
                 </div>
             </div>
-            <img class="post-image" src="./${this.post}">
+            <img class="post-image" src="./${this.post}" alt="post-image">
             <div id="icons-container">
                 <button>
-                    <img class="icons" src="./assets/hearth-logo.png">
+                    <img class="icons" src="./assets/hearth-logo.png" alt="hearth-logo">
                 </button>
                 <button>
-                    <img class="icons" id="comment-logo" src="./assets/comment-logo.png">
+                    <img class="icons" id="comment-logo" src="./assets/comment-logo.png" alt="comment-logo">
                 </button>
                 <button>
-                    <img class="icons" id ="send-logo" src="./assets/send-logo.png">
+                    <img class="icons" id ="send-logo" src="./assets/send-logo.png" alt="send-logo">
                 </button>
             </div>
             <h3>${this.likes} likes</h3>
-            <h4><strong>${this.username}</strong>,${this.comment}</h4>
+            <h4><strong>${this.username}</strong>     ${this.comment}</h4>
         </div>
         `
     }
 }
-
 customElements.define('oldagram-post', OldagramPost)
